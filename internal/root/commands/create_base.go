2dcfe772176a67bfc560a18308355b0175d37594
skit <drizzleent@yandex.ru>
2025-04-19 12:18:00 +0300

    [feature] add base project gen

---
+++ internal/root/commands/create_base.go
@@ -0,0 +1,58 @@
+package commands
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+func CreateBaseStruct(name string) {
+	dirs := []string{
+		filepath.Join(name, "cmd"),
+		filepath.Join(name, "internal"),
+		filepath.Join(name, "pkg"),
+		filepath.Join(name, "internal/service"),
+		filepath.Join(name, "internal/repository"),
+		filepath.Join(name, "internal/app"),
+		filepath.Join(name, "internal/domain"),
+	}
+
+	for _, dir := range dirs {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			log.Printf("failed create directory %s: %s\n", dir, err.Error())
+			return
+		}
+
+		log.Printf("directory %s created\n", dir)
+	}
+
+	mainFile := filepath.Join(name, "cmd", "main.go")
+	mainContent := `package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello from ` + name + `")
+}		
+	`
+	if err := os.WriteFile(mainFile, []byte(mainContent), 0644); err != nil {
+		log.Printf("failed create main file %s: %s\n", mainFile, err.Error())
+		return
+	}
+
+	log.Printf("main file created %s\n", mainFile)
+
+	goVer := runtime.Version()
+	goVer = strings.TrimPrefix(goVer, "go")
+
+	modFile := filepath.Join(name, "go.mod")
+	modContent := fmt.Sprintf("module %s\n\ngo %s\n", name, goVer)
+
+	if err := os.WriteFile(modFile, []byte(modContent), 0644); err != nil {
+		log.Printf("failed create gomod file %s: %s\n", modFile, err.Error())
+		return
+	}
+}
--- internal/root/root.go
+++ internal/root/root.go
@@ -1,21 +1,33 @@
 package root
 
 import (
+	"fmt"
+	"log"
 	"os"
 
+	"github.com/drizzleent/goprojectgen/internal/root/commands"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "prg",
 	Short: "golang project generation",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Привет! Используй `prg init <projectname>` чтобы создать проект.")
+	},
 }
 
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "init project",
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Flags().GetString("name")
+		name, err := cmd.Flags().GetString("name")
+		if err != nil {
+			log.Printf("failed to read name flag: %s", err.Error())
+			os.Exit(1)
+		}
+		commands.CreateBaseStruct(name)
+		fmt.Printf("created new project %s\n", name)
 	},
 }
 
@@ -29,4 +41,5 @@ func Execute() {
 func init() {
 	rootCmd.AddCommand(initCmd)
 
+	initCmd.Flags().StringP("name", "n", "app", "Project name")
 }
